2115e5f002e6ebe44bde242db8cf4ef6b486b4d8
Triet Nguyen <petertriet@gmail.com>
2020-11-03 09:58:04 +0700

    Update bkparser.py

--- bkparser.py
+++ bkparser.py
@@ -1,33 +1,44 @@
 from py4j.java_gateway import JavaGateway
 
-BKPARSER_JAR = "BKParser-1.0.jar"
-
-
-def get_bkparser(jar_file):
-    gateway = JavaGateway.launch_gateway(classpath=jar_file, die_on_exit=True)
-    parser = gateway.jvm.vn.edu.hust.nlp.parser.BKParser()
-    return parser
-
+class Token:
+    def __init__(self, conll):
+        # CoNLL-U Format
+        # https://universaldependencies.org/format.html
+        # id, form, lemma, uPOSTag, xPOSTag, feats, head, depRel, deps, misc
+        conll = conll.split("\t")
+        self.id = conll[0]
+        self.form = conll[1]
+        self.lemma = conll[2]
+        self.uPOSTag = conll[3]
+        self.xPOSTag = conll[4]
+        self.feats = conll[5]
+        self.head = conll[6]
+        self.depRel = conll[7]
+        self.deps = conll[8]
+        self.misc = conll[9]
 
 class BKParser:
-    def __init__(self, jar_file=BKPARSER_JAR):
+    def __init__(self, jar_file="BKParser-1.0.jar"):
         try:
-            self.__model = get_bkparser(jar_file)
+            self.__model = self.get_bkparser(jar_file)
         except:
             raise RuntimeError("Can not init model. Check log file!")
 
-    def parse(self, txt):
+    def get_bkparser(self, jar_file):
+        gateway = JavaGateway.launch_gateway(classpath=jar_file, die_on_exit=True)
+        parser = gateway.jvm.vn.edu.hust.nlp.parser.BKParser()
+        return parser
+
+    def parse(self, text):
         result = []
         res = self.__model.parse(text)
         for tokenList in res:
-            for token in tokenList:
-                # CoNLL-U Format
-                # https://universaldependencies.org/format.html
-                # id, form, lemma, uPOSTag, xPOSTag, feats, head, depRel, deps, misc
-                line = token.toString()
+            for tokenC in tokenList:
+                line = tokenC.toString()
                 if line == '':
                     continue
-                result.append(line.split("\t"))
+                token = Token(line)
+                result.append(token)
         return result
 
 
